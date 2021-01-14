95e0397529ed947cbb3bc7ceebf31478be6777e1
wireboy <wireboy@163.com>
2021-01-14 22:49:00 +0800

    update send email

--- download.js
+++ download.js
@@ -4,7 +4,6 @@ const path = require("path");
 const progressStream = require('progress-stream');
 const nodemailer = require('nodemailer');
 
-console.log(process.env.DOWNLOADURL)
 //下载 的文件 地址
 let fileURL = process.env.DOWNLOADURL;
 //下载保存的文件路径
@@ -20,22 +19,39 @@ const fileStream = fs.createWriteStream(tmpFileSavePath).on('error', function (e
     //下载完成后重命名文件
     fs.renameSync(tmpFileSavePath, fileSavePath);
     console.log('文件下载完成:', fileSavePath);
+    const readstream = fs.createReadStream(fileSavePath);
+	let i = 0;
+	console.time('readtime');
+	readstream.on('readable', () => {
+        let patchIndex = 0;
+		let chunk = readstream.read(1024 * 1024 * 10);
+		while (null !== chunk) {
+            patchIndex = patchIndex + 1;
+            let emailFile = fs.createWriteStream(fileSavePath+".email").on('finish',function(){
 
+            })
+            emailFile.write(chunk);
+            emailFile.end();
+            let msg = createEmailMessage(patchIndex+'_'+path.basename(fileURL),fileSavePath+".email");
+            console.log('Send Mail ' + patchIndex + 'times');
+            console.log(path.basename(fileURL));
+            transporter.sendMail(msg, (error, info) => {
+                if (error) {
+                    console.log('Error occurred');
+                    console.log(error.message);
+                    return;
+                }
+                console.log('Message sent successfully!');
+                console.log('Server responded with "%s"', info.response);
+                transporter.close();
+            });
 
-    console.log('Send Mail');
-    console.log(path.basename(fileURL));
-    transporter.sendMail(message, (error, info) => {
-        if (error) {
-            console.log('Error occurred');
-            console.log(error.message);
-            return;
-        }
-        console.log('Message sent successfully!');
-        console.log('Server responded with "%s"', info.response);
-        transporter.close();
-    });
-
-
+			chunk = readstream.read(1024 * 1024 * 10);
+		}
+	});
+	readstream.on('close', () => {
+		console.timeEnd('readtime');
+	});
 });
 //请求文件
 fetch(fileURL, {
@@ -97,44 +113,50 @@ var transporter = nodemailer.createTransport({
 
 console.log('SMTP Configured');
 
-var message = {
-    // Comma separated lsit of recipients 收件人用逗号间隔
-    to: process.env.TOEMAIL,
-
-    // Subject of the message 信息主题
-    subject:  'Nodemailer is unicode friendly',
-
-    // plaintext body
-    text: 'Hello to myself~',
-
-    // Html body
-    html: '<p>成功下载文件</p>',
-
-    // Apple Watch specific HTML body 苹果手表指定HTML格式
-    watchHtml: '<b>Hello</b> to myself',
-
-    // An array of attachments 附件
-    attachments: [
-        // String attachment
-       //  {
-       //      filename: 'notes.txt',
-       //      content: 'Some notes about this e-mail',
-       //      contentType: 'text/plain' // optional,would be detected from the filename 可选的，会检测文件名
-       //  },
-       //  // Binary Buffer attchment
-       //  {
-       //      filename: 'image.png',
-       //      content: Buffer.from('iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD/' +
-       //         '//+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83NDDeNGe4U' +
-       //         'g9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC', 'base64'),
-       //      cid: '00001'  // should be as unique as possible 尽可能唯一
-       //  },
-        // File Stream attachment
-        {
-            filename: path.basename(fileURL),
-            path: fileSavePath,
-            // cid: '00002'  // should be as unique as possible 尽可能唯一
-         }
-    ]
-
+var createEmailMessage = function(filename,filepath){
+    var message = {
+        // Comma separated lsit of recipients 收件人用逗号间隔
+        to: process.env.TOEMAIL,
+    
+        // Subject of the message 信息主题
+        subject:  'Nodemailer is unicode friendly',
+    
+        // plaintext body
+        text: 'Hello to myself~',
+    
+        // Html body
+        html: '<p><b>Hello</b> to myself <img src= "cid:00001"/></p>' + 
+            '<p>Here\'s a nyan car for you as embedded attachment:<br/><img src="cid:00002"/></p>',
+    
+        // Apple Watch specific HTML body 苹果手表指定HTML格式
+        watchHtml: '<b>Hello</b> to myself',
+    
+        // An array of attachments 附件
+        attachments: [
+            // String attachment
+           //  {
+           //      filename: 'notes.txt',
+           //      content: 'Some notes about this e-mail',
+           //      contentType: 'text/plain' // optional,would be detected from the filename 可选的，会检测文件名
+           //  },
+           //  // Binary Buffer attchment
+           //  {
+           //      filename: 'image.png',
+           //      content: Buffer.from('iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD/' +
+           //         '//+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83NDDeNGe4U' +
+           //         'g9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC', 'base64'),
+           //      cid: '00001'  // should be as unique as possible 尽可能唯一
+           //  },
+            // File Stream attachment
+            {
+                filename: filename,
+                path: filepath,
+                // cid: '00002'  // should be as unique as possible 尽可能唯一
+             }
+        ]
+    
+    };
+    return message;
 };
+
+
