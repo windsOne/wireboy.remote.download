1b515a430165b73eaf654daa183a5e6cfd0165f3
wireboy <wireboy@163.com>
2021-01-15 00:30:46 +0800

    Update download.js
    测试源码下载

--- download.js
+++ download.js
@@ -6,7 +6,7 @@ const nodemailer = require('nodemailer');
 
 
 //下载 的文件 地址 （https://nodejs.org/dist/v12.18.3/node-v12.18.3-x64.msi）
-let fileURL = 'https://nodejs.org/dist/v12.18.3/node-v12.18.3-x64.msi';
+let fileURL = 'https://github.com/hemaju/Wireboy.Socket.P2PSocket/archive/master.zip';
 
 
 //下载保存的文件路径
